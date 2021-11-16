f55ad113c139a9f0e0ba0ab50c546686c574134a
Quentin Rampteau <quentin425@hotmail.fr>
2021-11-16 18:56:42 +0100

    Resolution issue#2 V2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -6,12 +6,29 @@ function editNav() {
     x.className = "topnav";
   }
 }
+// Regex
+const regexEmail = /^[0-9a-zA-Z-._]{1,64}@[a-zA-Z0-9]{1,64}.[a-z]{1,64}$/;
+const regexBirthdate = /^[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$/;
+const regexQuantity = /^[0-9]{1,2}$/;
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
+const submit = document.getElementById("submit");
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const location1 = document.getElementById("location1");
+const location2 = document.getElementById("location2");
+const location3 = document.getElementById("location3");
+const location4 = document.getElementById("location4");
+const location5 = document.getElementById("location5");
+const location6 = document.getElementById("location6");
+const checkbox1 = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -27,4 +44,34 @@ modalClose.addEventListener("click", closeModal);
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+// submit event
+submit.addEventListener("click", validate);
+
+//submit function
+async function validate(){
+  window.alert("lancement validate");
+  if (first.value.length < 2 ) {
+    window.alert("first pas bon");
+  }
+  if (last.value.length < 2 ) {
+    window.alert("last pas bon");
+  }
+  if (!(regexEmail.test(email.value))) {
+    window.alert("email pas bon");
+  }  
+  if (!(regexBirthdate.test(birthdate.value))) {
+    window.alert("birthdate pas bon");  
+  }
+  if (!(regexQuantity.test(quantity.value))) {
+    window.alert("quantity pas bon");
+  }
+  if (!(location1.checked || location2.checked || location3.checked || location4.checked || location5.checked || location6.checked)) {
+    window.alert("location pas bon");
+  }
+  if (!(checkbox1.checked)) {
+    window.alert("conditions pas bon");
+  }
+  window.alert("fin validate");
 }
