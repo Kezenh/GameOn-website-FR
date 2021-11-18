c731968f1289658f01c524ca851b343dc92bd455
Quentin Rampteau <quentin425@hotmail.fr>
2021-11-18 15:02:44 +0100

    Resolution issue#3#4

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,10 +12,12 @@ const regexBirthdate = /^[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$/;
 const regexQuantity = /^[0-9]{1,2}$/;
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalClose = document.querySelector(".close");
+const modal = document.getElementById("modal");
+const modalBtn = document.getElementById("modalBtn");
+const modalClose = document.getElementById("closeModal");
+const thanks = document.getElementById("thanks");
+const thanksClose1 = document.getElementById("thanksClose1");
+const thanksClose2 = document.getElementById("thanksClose2");
 const submit = document.getElementById("submit");
 const first = document.getElementById("first");
 const last = document.getElementById("last");
@@ -29,49 +31,141 @@ const location4 = document.getElementById("location4");
 const location5 = document.getElementById("location5");
 const location6 = document.getElementById("location6");
 const checkbox1 = document.getElementById("checkbox1");
+const firstError = document.getElementById("firstError");
+const lastError = document.getElementById("lastError");
+const emailError = document.getElementById("emailError");
+const birthdateError = document.getElementById("birthdateError");
+const quantityError = document.getElementById("quantityError");
+const locationError = document.getElementById("locationError");
+const checkbox1Error = document.getElementById("checkbox1Error");
+
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.addEventListener("click", launchModal);
 
-// launch modal form
+// launch modal function
 function launchModal() {
-  modalbg.style.display = "block";
+  modal.style.display = "block";
 }
 
 // close modal event
 modalClose.addEventListener("click", closeModal);
 
-// close modal form
+// close modal function
 function closeModal() {
-  modalbg.style.display = "none";
+  modal.style.display = "none";
+}
+
+// launch thanks function
+function launchThanks() {
+  thanks.style.display = "block";
+}
+
+// close thanks events
+thanksClose1.addEventListener("click", closeThanks);
+thanksClose2.addEventListener("click", closeThanks);
+
+// close thanks function
+function closeThanks() {
+  thanks.style.display = "none";
 }
 
 // submit event
 submit.addEventListener("click", validate);
 
 //submit function
-async function validate(){
-  window.alert("lancement validate");
+function validate(){
+  let close = true;
   if (first.value.length < 2 ) {
     window.alert("first pas bon");
+    firstError.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+    first.style.border = "#e54858 solid 2px";
+    close = false;
   }
   if (last.value.length < 2 ) {
     window.alert("last pas bon");
+    lastError.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    last.style.border = "#e54858 solid 2px";
+    close = false;
   }
   if (!(regexEmail.test(email.value))) {
     window.alert("email pas bon");
+    emailError.innerText = "Vous devez entrer un email valide.";
+    email.style.border = "#e54858 solid 2px";
+    close = false;
   }  
   if (!(regexBirthdate.test(birthdate.value))) {
     window.alert("birthdate pas bon");  
+    birthdateError.innerText = "Vous devez entrer votre date de naissance.";
+    birthdate.style.border = "#e54858 solid 2px";
+    close = false;
   }
   if (!(regexQuantity.test(quantity.value))) {
     window.alert("quantity pas bon");
+    quantityError.innerText = "Veuillez entrer un nombre entre 0 et 99.";
+    quantity.style.border = "#e54858 solid 2px";
+    close = false;
   }
   if (!(location1.checked || location2.checked || location3.checked || location4.checked || location5.checked || location6.checked)) {
     window.alert("location pas bon");
+    locationError.innerText = "Vous devez choisir une option.";
+    close = false;
   }
   if (!(checkbox1.checked)) {
     window.alert("conditions pas bon");
+    checkbox1Error.innerText = "Vous devez vérifier que vous acceptez les termes et conditions.";
+    close = false;
   }
-  window.alert("fin validate");
+  if (close == true) {
+    closeModal();
+    launchThanks();
+  }
+}
+
+//reset events
+first.addEventListener("input", firstReset);
+last.addEventListener("input", lastReset);
+email.addEventListener("input", emailReset);
+birthdate.addEventListener("input", birthdateReset);
+quantity.addEventListener("input", quantityReset);
+location1.addEventListener("input", locationReset);
+location2.addEventListener("input", locationReset);
+location3.addEventListener("input", locationReset);
+location4.addEventListener("input", locationReset);
+location5.addEventListener("input", locationReset);
+location6.addEventListener("input", locationReset);
+checkbox1.addEventListener("input", checkbox1Reset);
+
+//reset functions
+function firstReset() {
+  firstError.innerText = "";
+  first.style.border = "none";
+}
+
+function lastReset() {
+  lastError.innerText = "";
+  last.style.border = "none";
+}
+
+function emailReset() {
+  emailError.innerText = "";
+  email.style.border = "none";
+}
+
+function birthdateReset() {
+  birthdateError.innerText = "";
+  birthdate.style.border = "none";
+}
+
+function quantityReset() {
+  quantityError.innerText = "";
+  quantity.style.border = "none";
+}
+
+function locationReset() {
+  locationError.innerText = "";
+}
+
+function checkbox1Reset() {
+  checkbox1Error.innerText = "";
 }
